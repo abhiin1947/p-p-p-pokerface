e940e0a1ceba68ebbbfcdf3668ae4a0b4029d598
Abhinandan <abhiin1947@gmail.com>
2014-07-28 16:24:06 +0530

    all checks succeeded

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,66 @@
 (ns p-p-p-pokerface)
 
+(def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[rank suit] card]
+    (if (Character/isDigit rank) (Integer/valueOf (str rank)) (get replacements rank))))
 
 (defn suit [card]
-  nil)
+  (let [[rank suit] card]
+    (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (> (count (filter (fn [x] (= x 2)) (vals (frequencies (map rank hand))))) 0))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (> (count (filter (fn [x] (= x 3)) (vals (frequencies (map rank hand))))) 0))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (> (count (filter (fn [x] (= x 4)) (vals (frequencies (map rank hand))))) 0))
 
 (defn flush? [hand]
-  nil)
+  (> (count (filter (fn [x] (= x 5)) (vals (frequencies (map suit hand))))) 0))
 
 (defn full-house? [hand]
-  nil)
+  (and (three-of-a-kind? hand) (pair? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (> (count (filter (fn [x] (= x 2)) (vals (frequencies (map rank hand))))) 1) (four-of-a-kind? hand)))
 
 (defn straight? [hand]
-  nil)
+  (let [otherhand (sort
+                   (replace
+                    {14 1}
+                    (map rank hand)))
+        hand2 (sort
+               (map rank hand))
+        gen-range (fn [x]
+                    (range
+                     (apply min x)
+                     (+ (apply min x) 5)))]
+    (or
+     (=
+      (gen-range hand2)
+      hand2)
+     (=
+      (gen-range otherhand)
+      otherhand))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand)   8
+   (four-of-a-kind? hand)   7
+   (full-house? hand)       6
+   (flush? hand)            5
+   (straight? hand)         4
+   (three-of-a-kind? hand)  3
+   (two-pairs? hand)        2
+   (pair? hand)             1
+   (high-card? hand)        0))
